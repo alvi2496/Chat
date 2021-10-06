afdbbf85fc631d21de05cb08cea54f0a73255c53
Alvi Mahadi <support@appsbypix.com>
2021-10-05 17:53:38 -0700

    Complete draft of messageItem

--- src/app/_components/MessageList/MessageItem.js
+++ src/app/_components/MessageList/MessageItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col } from "antd";
 import { Paper } from "@mui/material";
 
 export default function MessageItem({
@@ -9,21 +10,35 @@ export default function MessageItem({
     paper: {
       maxWidth: "50%",
       width: "fit-content",
-      float: direction === "incoming" ? "left" : "right"
+      float: direction === "incoming" ? "left" : "right",
+      backgroundColor: direction === "incoming" ? "white" : "rgb(245, 245, 245)"
     },
     content: {
-      paddingTop: "5px",
-      paddingBottom: "5px",
+      paddingTop: "10px",
       paddingLeft: "15px",
-      paddingRight: "15px"
+      paddingRight: "15px",
+      // padding: "10px",
+      textAlign: "center"
     }
   }
 
   return (
-    <div>
-      <Paper elevation={5} style={styles.paper}>
-        <p style={styles.content}>{message}</p>
-      </Paper>
-    </div>
+
+    direction === "incoming" ?
+      <Row>
+        <Col span={24}>
+          <Paper elevation={1} style={styles.paper}>
+            <p style={styles.content}>{message}</p>
+          </Paper>
+        </Col>
+      </Row>
+      :
+      <Row>
+        <Col span={24}>
+          <Paper elevation={1} style={styles.paper}>
+            <p style={styles.content}>{message}</p>
+          </Paper>
+        </Col>
+      </Row>
   )
 }
--- src/app/_components/MessageList/index.js
+++ src/app/_components/MessageList/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import MessageItem from "./MessageItem"
+// import { List, ListItem } from "@mui/material";
+import { List } from "antd";
 
 export default function MessageList({
   messages
 }) {
+
+
+
+
   return (
-    <div>
-      {
-        messages.map((message) => (
-          <MessageItem
-            key={`message-${message.id}`}
-            message={message.body}
-            direction={message.direction}
-          />
-        ))
-      }
-    </div>
+    messages.map((message) => (
+        <MessageItem
+          key={`message-${message.id}`}
+          message={message.body}
+          direction={message.direction}
+        />
+    ))
   )
 }
